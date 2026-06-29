4fd79a45b250278de0ea63f4f4aa892855e12079
Franz Wendel <franz.wendel21@gmail.com>
2026-06-30 00:42:26 +0200

    Change comments in model scripts

--- 03_geographic_correlation.jl
+++ 03_geographic_correlation.jl
@@ -15,7 +15,7 @@ data_dir = joinpath(@__DIR__, "data")
 version = "_03_geographic_correlation"
 
 ### Read in of parameters ###
-# We define our sets from the csv files
+# sets from csv files
 technologies = readcsv("technologies.csv", dir=data_dir).technology
 fuels = readcsv("fuels.csv", dir=data_dir).fuel
 storages = readcsv("storages.csv", dir=data_dir).storage
@@ -23,10 +23,10 @@ hour = 1:120
 n_hour = length(hour)
 regions = readcsv("regions.csv", dir=data_dir).region
 
-# also, we need a set for our years
+# years
 year = 2020:10:2050
 
-# Also, we read our input parameters via csv files
+# input parameters from csv files
 Demand = readin("demand_regions.csv", dims=3, dir=data_dir)
 OutputRatio = readin("outputratio.csv", dims=2, dir=data_dir)
 InputRatio = readin("inputratio.csv", dims=2, dir=data_dir)
@@ -41,10 +41,10 @@ TagRenewable = readin("tagrenewable.csv",default=0, dims=1, dir=data_dir)
 TagElectrolyzer = readin("tagelectrolyzer.csv", default=0, dims=1, dir=data_dir)
 
 
-# we need to read in region and year dependent unmetdemandpenalty
+# region- and year-dependent unmet demand penalty
 UnmetDemandPenalty = readin("unmetdemandpenalty.csv", default=100, dims=2, dir=data_dir)
 
-# we need to ensure that all non-variable technologies do have a CapacityFactor of 1 at all times
+# dispatchable technologies have a capacity factor of 1 at all times
 for r in regions    
     for t in technologies
         if TagDispatchableTechnology[t] > 0
@@ -60,7 +60,7 @@ for r in regions
 end
 
 
-### Also, we need to read in our additional storage parameters
+### storage parameters
 InvestmentCostStorage = readin("investmentcoststorage.csv",dims=2, dir=data_dir)
 E2PRatio = readin("e2pratio.csv",dims=1, dir=data_dir)
 StorageChargeEfficiency = readin("storagechargeefficiency.csv",dims=2, dir=data_dir)
@@ -68,17 +68,17 @@ StorageDischargeEfficiency = readin("storagedischargeefficiency.csv",dims=2, dir
 MaxStorageCapacity = readin("maxstoragecapacity.csv",default=50,dims=3, dir=data_dir)
 StorageLosses = readin("storagelosses.csv",default=1,dims=2, dir=data_dir)
 
-# our yearly emission limit
+# yearly emission limit
 AnnualEmissionLimit = readin("annualemissionlimit.csv",default=999999,dims=1, dir=data_dir)
 RegionalAnnualEmissionLimit = readin("regionalannualemissionlimit.csv",default=999999,dims=2, dir=data_dir)
 
-#our discount rate
+# discount rate
 DiscountRate = 0.05
 
-# stuff for emission trajectories
+# model-period emission limit
 ModelPeriodEmissionLimit = 25500000
 
-# create a multiplier to weight the different years correctly
+# multiplier to weight the gap between modelled years
 YearlyDifferenceMultiplier = Dict()
 for i in 1:length(year)-1
     difference = year[i+1] - year[i]
@@ -86,16 +86,15 @@ for i in 1:length(year)-1
     YearlyDifferenceMultiplier[year[i]] = difference
 end
 YearlyDifferenceMultiplier[year[end]] = 1
-# this gives us the distance between each year for all years
 YearlyDifferenceMultiplier
 
-# define the dictionary for max capacities with specific default value
+# maximum capacity (with default)
 MaxCapacity = readin("maxcapacity.csv", default=999, dims=3, dir=data_dir)
 MaxTradeCapacity = readin("maxtradecapacity.csv", default=0, dims=4, dir=data_dir)
 ResidualCapacity = readin("residualcapacity.csv", default=0, dims=3, dir=data_dir)
 TechnologyLifetime = readin("technologylifetime.csv", default=10, dims=1, dir=data_dir)
 
-# add your trade distances and other trade parameters
+# trade parameters
 TradeDistance = readin("tradedistance.csv",default=0,dims=2, dir=data_dir)
 TradeCostFactor = readin("tradecostfactor.csv",default=0,dims=1, dir=data_dir)
 TradeLossFactor = readin("tradelossfactor.csv",default=0,dims=1, dir=data_dir)
@@ -105,7 +104,7 @@ TradeLossFactor = readin("tradelossfactor.csv",default=0,dims=1, dir=data_dir)
 ESM = Model(Gurobi.Optimizer)
 set_optimizer_attribute(ESM, "Threads", 3)
 
-# this creates our variables
+# decision variables
 @variable(ESM, TotalCost[year,regions,technologies] >= 0)
 @variable(ESM, FuelProductionByTechnology[year,regions,hour,t = technologies,f = fuels; OutputRatio[t,f]>0] >= 0)
 @variable(ESM, NewCapacity[year,regions,technologies] >=0)
@@ -114,7 +113,7 @@ set_optimizer_attribute(ESM, "Threads", 3)
 @variable(ESM, AnnualTechnologyEmissions[year,regions,technologies])
 @variable(ESM, Curtailment[year,regions,hour,fuels] >=0)
 
-### And we also need to add our new variables for storages
+### storage variables
 @variable(ESM, NewStorageEnergyCapacity[year,regions,s=storages,f=fuels; StorageDischargeEfficiency[s,f]>0]>=0)
 @variable(ESM, AccumulatedStorageEnergyCapacity[year,regions,s=storages,f=fuels; StorageDischargeEfficiency[s,f]>0]>=0)
 @variable(ESM, StorageCharge[year,regions,s=storages, hour, f=fuels; StorageDischargeEfficiency[s,f]>0]>=0)
@@ -122,13 +121,13 @@ set_optimizer_attribute(ESM, "Threads", 3)
 @variable(ESM, StorageLevel[year,regions,s=storages, hour, f=fuels; StorageDischargeEfficiency[s,f]>0]>=0)
 @variable(ESM, TotalStorageCost[year,regions,storages] >= 0)
 
-##### now, we also need new variables for trade between regions
+##### trade variables
 @variable(ESM, Import[y = year,r = regions,rr = regions,hour,f = fuels; MaxTradeCapacity[y,r,rr,f]>0] >= 0)
 @variable(ESM, Export[y = year,r = regions,rr = regions,hour,f = fuels; MaxTradeCapacity[y,r,rr,f]>0] >= 0)
 
 @variable(ESM, SalvageValue[year,regions,technologies] >= 0)
 
-# we introduce a new variable to catch a lack in supply options, we will call this UnmetDemand
+# unmet demand variable to catch a lack of supply options
 @variable(ESM, UnmetDemand[year,regions,hour,fuels] >= 0)
 
 #@variable(ESM, CapacitySlack[year,regions,technologies] >= 0)
@@ -139,7 +138,7 @@ set_optimizer_attribute(ESM, "Threads", 3)
     (sum(FuelProductionByTechnology[y,r,h,t,f] for t in technologies if OutputRatio[t,f]>0) 
     + sum(StorageDischarge[y,r,s,h,f] for s in storages if StorageDischargeEfficiency[s,f]>0) 
     + sum(Import[y,r,rr,h,f] for rr in regions if MaxTradeCapacity[y,r,rr,f]>0))*8760/n_hour
-    # we can now add UnmetDemand to the production side of the energy balance
+    # UnmetDemand on the production side of the energy balance
     + UnmetDemand[y,r,h,f] == 
     Demand[y,r,f]*DemandProfile[r,h,f] 
     + (sum(FuelUseByTechnology[y,r,h,t,f] for t in technologies if InputRatio[t,f]>0) 
@@ -178,7 +177,7 @@ set_optimizer_attribute(ESM, "Threads", 3)
     sum(AnnualTechnologyEmissions[y,r,t] for t in technologies for r in regions) <= AnnualEmissionLimit[y]
 )
 
-# now also limit the emissions in each individual year for each region
+# limit the emissions per year and region
 @constraint(ESM, RegionalAnnualEmissionsLimitFunction[y in year,r in regions],
     sum(AnnualTechnologyEmissions[y,r,t] for t in technologies) <= RegionalAnnualEmissionLimit[y,r]
 )
@@ -188,7 +187,7 @@ set_optimizer_attribute(ESM, "Threads", 3)
      AccumulatedCapacity[y,r,t] <= MaxCapacity[y,r,t]
 )
 
-### Add your storage constraints here
+### storage constraints
 @constraint(ESM, StorageChargeFunction[y in year,r in regions,s in storages, h in hour, f in fuels; StorageDischargeEfficiency[s,f]>0], 
     StorageCharge[y,r,s,h,f] <= AccumulatedStorageEnergyCapacity[y,r,s,f]/E2PRatio[s]
 )
@@ -256,9 +255,9 @@ set_optimizer_attribute(ESM, "Threads", 3)
 )
 
 
-#### new constraint for termpaper ####
+#### regulatory constraints ####
 
-#nur erneuerbaren Strom verwenden für Wasserstoff
+# use only renewable power for hydrogen
 @constraint(ESM, RenewablePowerForHydrogen[y in year, h in hour, r in regions, f in fuels; f == "Power"],
     sum(FuelProductionByTechnology[y,r,h,t,f]
         for t in technologies if TagRenewable[t] == 1) >=
@@ -273,12 +272,12 @@ set_optimizer_attribute(ESM, "Threads", 3)
         for t in technologies if TagRenewable[t] == 1)
 )    
 
-#Beschränkung des jährlichen Stromverbrauchs der neuen Elektrolysekapazitäten druch die in diesem Jahr verfügabaren Kapazitäten
+# limit the annual electricity use of new electrolyser capacities to the renewable capacity available that year
 @constraint(ESM, NewElectrolyzeRequiresNewRE[y in year, h in hour, r in regions, f in fuels; f == "Power"],
             sum(NewCapacity[y,r,t] * CapacityFactor[r,h,t] * OutputRatio[t,f] * 8760/n_hour
                 for t in technologies if TagRenewable[t] == 1) >=
             sum(FuelUseByTechnology[y,r,h,t,f] - InputRatio[t,f] *
-            (AccumulatedCapacity[y,r,t] - NewCapacity[y,r,t]) * CapacityFactor[r,h,t] #Stromverbrauch der neuen Elektrolysekapazitäten
+            (AccumulatedCapacity[y,r,t] - NewCapacity[y,r,t]) * CapacityFactor[r,h,t] # electricity consumption of the new electrolyser capacities
                 for t in technologies if TagElectrolyzer[t] == 1)* 8760/n_hour
             
 )
@@ -290,17 +289,16 @@ set_optimizer_attribute(ESM, "Threads", 3)
     + sum(TotalStorageCost[y,r,s]/ (1+DiscountRate)^(y - minimum(year)) for s in storages for r in regions, y in year)
     + sum(TradeCostFactor[f]*TradeDistance[r,rr] * Export[y,rr,r,h,f]  * YearlyDifferenceMultiplier[y] / (1+DiscountRate)^(y - minimum(year)) for h in hour, r in regions, rr in regions, f in fuels, y in year if MaxTradeCapacity[y,r,rr,f]>0)
     - sum(SalvageValue[y,r,t] for y in year, r in regions, t in technologies)
-    # now, UnmetDemand should also be punished properly so that the model does not try to use it for no reason
+    # penalise unmet demand so it is only used when unavoidable
     + sum(UnmetDemand[y,r,h,f]*UnmetDemandPenalty[y,r] for y in year, r in regions, h in hour, f in fuels)
     )
 
-# this starts the optimization
-# the assigned solver (here Gurobi) will takes care of the solution algorithm
+# solve the model
 optimize!(ESM)
-# reading our objective value
+# objective value
 objective_value(ESM)
 
-# we can also analyze specific equations and their marginals / shadow values
+# marginals / shadow values of selected emission constraints
 println("sum of all technology emissions per year")
 println(value.(AnnualEmissionsLimitFunction))
 println("marginals for constraint AnnualEmissionsLimitFunction")
@@ -317,8 +315,7 @@ value.(SalvageValue)
 sum(value.(UnmetDemand))
 sum(value.(FuelUseByTechnology)*8760/n_hour)
 
-# we will analyze the results in Tableau from now on, so we just need to write our results to csv files
-# you can find the code that does this reading, merging, and writing of results in the helper_functions.jl file!
+# write results to csv files
 df_energybalance,df_capacity,df_storage_level,df_trade,df_emissions = process_dataframes()
 write_result_csvs("",version)
 
